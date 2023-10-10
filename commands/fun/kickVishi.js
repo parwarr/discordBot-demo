f8b589482239b8f779be128095db588b2020e5be
parwarr <parwar.habasch@gmx.de>
2023-10-11 00:40:38 +0200

    feat: update project

--- commands/fun/kickVishi.js
+++ commands/fun/kickVishi.js
@@ -3,13 +3,17 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('vishi')
+        .addUserOption(option => option.setName('user').setDescription('The user to kick'))
         .setDescription('Kick Vishi!'),
-    async disconnectUser(userID, interaction){
+        async disconnectUser(userID, interaction){
         let user = await interaction.guild.members.fetch(userID);
-        user.voice.disconnect();
+        await user.voice.disconnect();
+            
     },
     async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        if (user.id !== '531105038609809419') return interaction.reply('This is not Vishi! You only can kick vishi with this command!');
+        await this.disconnectUser(user.id, interaction);
         await interaction.reply('Vishi has been kicked from the call!');
-        this.disconnectUser('531105038609809419', interaction);
-    },
+        },
 };
